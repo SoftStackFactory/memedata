0265c6aae4a4b3bdebf81393b11671921af98bca
bfriend <stokelvlmidnight@gmail.com>
2019-05-01 20:53:29 -0700

    quick update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
+import { PollHistoryPage } from '../pages/poll-history/poll-history';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = PollHistoryPage;
 
   pages: Array<{title: string, component: any}>;
 
