cdfd84398075095b364b779686c1904b8e2172b1
Michael Peterson <michaelpp2001@gmail.com>
2019-05-06 17:08:37 -0700

    added delete function on the builder 3 page

--- src/pages/poll-builder3/poll-builder3.ts
+++ src/pages/poll-builder3/poll-builder3.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PollBuilderPage } from '../poll-builder/poll-builder';
 import { PollBuilder4Page } from '../poll-builder4/poll-builder4';
 import { PollBuilderServiceProvider } from '../../providers/poll-builder-service/poll-builder-service';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the PollBuilder3Page page.
@@ -19,13 +20,38 @@ import { PollBuilderServiceProvider } from '../../providers/poll-builder-service
 })
 export class PollBuilder3Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public BuilderService: PollBuilderServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public BuilderService: PollBuilderServiceProvider, public alertCtrl: AlertController) {
   }
 
   previewMeme(meme){
     console.log("clicked", meme.topText);
     this.BuilderService.displayMeme = meme; 
   }
+
+  deleteMeme(meme){
+    console.log("deleted", meme.topText);
+    let confirm = this.alertCtrl.create({
+      title: 'Delete forever?',
+      message: 'Are you sure you want to permanently delete this meme?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            let result = this.BuilderService.memes.splice(this.BuilderService.memes.findIndex(find => find.image === meme.image), 1);
+            console.log("deleted", result[0].topText);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+  
 
   addNextMeme() {
     this.navCtrl.push(PollBuilderPage);
