b13c4b0709f60555535f3168a92ca6144f411fab
Michael Peterson <michaelpp2001@gmail.com>
2019-05-08 20:58:52 -0700

    moved create poll button

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PollBuilderPage } from '../poll-builder/poll-builder';
+import { PollBuilderServiceProvider } from '../../providers/poll-builder-service/poll-builder-service';
 
 /**
  * Generated class for the DashboardPage page.
@@ -15,11 +17,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DashboardPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public BuilderService: PollBuilderServiceProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DashboardPage');
   }
+
+  goPollBuilder() {
+    this.BuilderService.createPollSet()
+    .subscribe(
+      (response: any) => {
+        console.log("New PollSet", response);
+        this.BuilderService.pollSets = response
+        this.BuilderService.pollId = response.id
+        this.BuilderService.pollSets.userId = this.BuilderService.userId
+      }
+    )
+    this.navCtrl.push(PollBuilderPage);
+}
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { PollBuilderPage } from '../poll-builder/poll-builder';
 import { PollBuilderServiceProvider } from '../../providers/poll-builder-service/poll-builder-service';
 
 @Component({
@@ -12,18 +11,5 @@ export class HomePage {
   constructor(public navCtrl: NavController, public BuilderService: PollBuilderServiceProvider) {
 
   }
-
-  goPollBuilder() {
-    this.BuilderService.createPollSet()
-    .subscribe(
-      (response: any) => {
-        console.log("New PollSet", response);
-        this.BuilderService.pollSets = response
-        this.BuilderService.pollId = response.id
-        this.BuilderService.pollSets.userId = this.BuilderService.userId
-      }
-    )
-    this.navCtrl.push(PollBuilderPage);
-}
 
 }
