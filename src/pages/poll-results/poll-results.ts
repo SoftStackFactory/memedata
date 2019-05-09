f7b555652f2caaccae168bb70e3808703aa77d91
Paul Avila <pavil13@safeway.com>
2019-05-08 20:50:04 -0700

    Added suggested results bar and flip for more info on meme results.

--- src/pages/poll-results/poll-results.ts
+++ src/pages/poll-results/poll-results.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,13 +8,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PollResultsPage {
 
+  @ViewChild(Slides) slides: Slides;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
-  array:any = [1,2,3,4,5]
+  array:any = [1,2,3,4]
+  suggestions:any = [1,2,3,4,5,6,7,8,9,10]
+
+  left: any = 0
+  right: any = 0
+
+  bar1:number = 22
+  barPercent1:any
+  barPercent2:any
+  bar2:number = 100 - this.bar1
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PollResultsPage');
+
+    this.barPercent1 = {'width': this.bar1 + "%"}
+    this.barPercent2 = {'width': this.bar2 + "%"}
+
   }
+
+  swipeEvent(e) {
+    if (e.direction == "2") {
+      this.left++
+      console.log("Swipped Left " + this.left + " times.")
+    } else if ( e.direction == "4") {
+      this.right++
+      console.log("Swipped Right " + this.right + " times.")
+    }
+  }
+
+  slideTo(x) {
+    this.slides.slideTo(x)
+  }
+
 
 }
