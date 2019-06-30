d33d8fae41bc3fff922069f02d92130f0788ab9c
Michael Peterson <michaelpp2001@gmail.com>
2019-06-30 15:17:42 -0700

    fix bug where menu was being disable after pushing to dashboard, had to disable,then push to dash re-enable

--- src/components/searchbar/searchbar.ts
+++ src/components/searchbar/searchbar.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams, Nav } from 'ionic-angular';
+import { NavController, NavParams, Nav, MenuController } from 'ionic-angular';
 import { SearchbarServiceProvider } from '../../providers/searchbar-service/searchbar-service';
 import { FacebookOathProvider } from '../../providers/facebook-oath/facebook-oath';
 import { PollBuilderServiceProvider } from '../../providers/poll-builder-service/poll-builder-service';
@@ -30,9 +30,9 @@ declare var FB: any;
   templateUrl: 'searchbar.html'
 })
 export class SearchbarComponent {
-  @ViewChild(Nav) nav: Nav;
+  @ViewChild("Nav") nav: Nav;
 
-  rootPage: any;
+  rootPage: any = DashboardPage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -47,7 +47,8 @@ export class SearchbarComponent {
     public fb: Facebook,
     public fbOath: FacebookOathProvider,
     public alertCtrl: AlertController,
-    public BuilderService: PollBuilderServiceProvider
+    public BuilderService: PollBuilderServiceProvider,
+    public menuCtrl: MenuController
     ) {
     console.log('Hello SearchbarComponent');
     this.text = 'Hello World';
